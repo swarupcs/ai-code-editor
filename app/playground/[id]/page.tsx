64fe2aed83231461456237cdaa2b718ab038d508
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-10-02 22:04:47 +0530

    implement monaco-editor-part-1

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,11 +1,13 @@
-'use client';
-import { Separator } from '@/components/ui/separator';
-import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { TemplateFileTree } from '@/modules/playground/components/playground-explorer';
-import { usePlayground } from '@/modules/playground/hooks/usePlayground';
-import { useParams } from 'next/navigation';
-import React from 'react';
+"use client";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { TemplateFileTree } from "@/modules/playground/components/playground-explorer";
+import { useFileExplorer } from "@/modules/playground/hooks/useFileExplorer";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { TemplateFile } from "@/modules/playground/lib/path-to-json";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 const MainPlaygroundPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -13,19 +15,42 @@ const MainPlaygroundPage = () => {
   const { playgroundData, templateData, isLoading, error, saveTemplateData } =
     usePlayground(id);
 
-  console.log('templateData', templateData);
-  console.log('playgroundData', playgroundData);
+  const {
+    setTemplateData,
+    setActiveFileId,
+    setPlaygroundId,
+    setOpenFiles,
+    activeFileId,
+    closeAllFiles,
+    openFile,
+    openFiles,
+  } = useFileExplorer();
 
-  const activeFile = 'sample.txt';
+  useEffect(() => {
+    setPlaygroundId(id);
+  }, [id, setPlaygroundId]);
+
+  useEffect(() => {
+    if (templateData && !openFiles.length) {
+      setTemplateData(templateData);
+    }
+  }, [templateData, setTemplateData, openFiles.length]);
+
+  const activeFile = openFiles.find((file)=>file.id === activeFileId);
+  const hasUnsavedChanges = openFiles.some((file)=>file.hasUnsavedChanges);
+
+  const handleFileSelect = (file:TemplateFile){
+    openFile(file)
+  }
 
   return (
     <TooltipProvider>
       <>
         <TemplateFileTree
           data={templateData!}
-          onFileSelect={() => {}}
+          onFileSelect={handleFileSelect}
           selectedFile={activeFile}
-          title='File Explorer'
+          title="File Explorer"
           onAddFile={() => {}}
           onAddFolder={() => {}}
           onDeleteFile={() => {}}
@@ -34,15 +59,15 @@ const MainPlaygroundPage = () => {
           onRenameFolder={() => {}}
         />
         <SidebarInset>
-          <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
-            <SidebarTrigger className='-ml-1' />
-            <Separator orientation='vertical' className='mr-2 h-4' />
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
           </header>
 
-          <div className='flex flex-1 items-center gap-2'>
-            <div className='flex flex-col flex-1'>
-              <h1 className='text-sm font-medium'>
-                {playgroundData?.title || 'Code Playground'}
+          <div className="flex flex-1 items-center gap-2">
+            <div className="flex flex-col flex-1">
+              <h1 className="text-sm font-medium">
+                {playgroundData?.title || "Code Playground"}
               </h1>
             </div>
           </div>

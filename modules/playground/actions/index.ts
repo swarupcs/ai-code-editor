8d84e5ab6d6ceb3ee78b8ea34ccb64dfda1dd089
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-10-02 22:03:15 +0530

    Implement File-Explorer-2

--- modules/playground/actions/index.ts
+++ modules/playground/actions/index.ts
@@ -13,6 +13,7 @@ export const getPlaygroundById = async(id:string)=>{
         const playground = await db.playground.findUnique({
             where:{id},
             select:{
+                title:true,
                 templateFiles:{
                     select:{
                         content:true
